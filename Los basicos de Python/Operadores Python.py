c717346879f7bb342700e4f44797df0005b7e065
Ramsés Ortiz Castro <106187776+RamsesOrtiz36@users.noreply.github.com>
2022-09-20 12:39:27 -0700

    Tipos de variables en python

---
+++ Los basicos de Python/Operadores Python.py
@@ -0,0 +1,7 @@
+x= int(input("Ingresa un digito: "))
+#Determina si el digito se encuentra entre los primeros 5 numeros primos
+if x==2 or x==3 or x==5 or x==7 or x==11:
+    print("El digito pertenece a los primeros 5 numeros primos")
+else:
+    print("El digito no pertenece a los primeros 5 numeros primos")
+    
---
+++ Los basicos de Python/Variables.py
@@ -0,0 +1,13 @@
+x = 73
+
+y = 1 + x*2 # Variable de operación matematica
+z = ("hola a todos") #variable de caracteres
+w = input("ingresa tu color favorito ") #variable de ingreso de caracteres
+edad=  int (input ("Ingresa tu edad ")) # Variable de ingreso numerico entero
+peso = float (input("¿Cuánto pesas? ")) #variable de ingreso del tipo decimal
+
+#imprime datos
+
+print("El valor de x es =",x)
+print("El valor de y es =",y)
+print("Tu color favorito es ",w," tienes ",edad," años y pesas ",peso," kg" )
